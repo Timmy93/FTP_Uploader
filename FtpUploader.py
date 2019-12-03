12d6b3f5cd568f84eaa0379fcde49ead78396987
Timmy93 <masomeno2006@gmail.com>
2019-12-03 15:00:12 +0100

    Added logging handler + bugfix

--- FtpUploader.py
+++ FtpUploader.py
@@ -5,17 +5,24 @@ import os
 #Allows to upload to the 1fichier account
 class FtpUploader:
 	
-	def __init__(self, host, username, passwordFile, port):	
+	def __init__(self, host, username, passwordFile, port=21):	
 		#Retrieve password
 		with open(passwordFile) as fp:
 			password = fp.readline().strip()
 		#Enstablish connection
-		self.connection = ftplib.FTP_TLS(host, username, password)
+		self.connection = ftplib.FTP_TLS(source_address=(host, port), username, password)
 		#Print welcome
 		print(self.connection.getwelcome())
 	
+	#Assign a logging handler
+	def addLogging(self, logging):
+		self.logging = logging;
+		self.logging.info("FtpUploader - Assigned logging handler")
+			
+	#Upload a file
 	def uploadFile(self, filePath):
-		filePath = os.path.basename(filePath)
+		name = os.path.basename(filePath)
 		with open(filePath, 'rb') as fp:
 			self.connection.storbinary('STOR '+name, fp)
-	
+		if (self.logging): 
+			self.logging.info("FtpUploader - uploadFile - Uploaded ["+name+"]")
