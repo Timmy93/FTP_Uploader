2fd1083e164568231293d2aff8a9cca0aa590d61
Federico <Timmy93@users.noreply.github.com>
2021-09-10 09:40:13 +0200

    Update FtpUploader.py

--- FtpUploader.py
+++ FtpUploader.py
@@ -1,60 +1,70 @@
-import requests
 import ftplib
 import os
 import time
 import sys
 
-#Allows to upload to the 1fichier account
+
 class FtpUploader:
+	"""
+	Allows to upload to a FTP account
+	"""
 	
 	def __init__(self, host, username, passwordFile, port=21, logging=None):
 		if logging:
-			self.addLogging(logging)
+			self.logging = logging
 		#Retrieve password
 		with open(passwordFile) as fp:
 			password = fp.readline().strip()
+
 		#Enstablish connection
 		self.connection = ftplib.FTP_TLS()
 		self.connection.connect(host, port)
-		if (self.logging): 
+		if self.logging:
 			self.logging.info("FtpUploader - uploadFile - Connected to ["+host+"]")
 		self.connection.login(username, password)
-		if (self.logging): 
+		if self.logging:
 			self.logging.info("FtpUploader - uploadFile - Logged as ["+username+"]")
 		self.connection.prot_p()
-		if (self.logging): 
+		if self.logging:
 			self.logging.info("FtpUploader - uploadFile - Connection secured")
-	
-	#Assign a logging handler
+
+		self.writtenSize = 0
+		self.maxSpeed = 0
+		self.uploadStart = None
+
 	def addLogging(self, logging):
-		self.logging = logging;
+		"""
+		Assign a logging handler
+		"""
+		self.logging = logging
 		if logging:
 			self.logging.info("FtpUploader - Assigned logging handler")
 		else:
 			print("No logging handler given")
-			
-	#Upload a file - Return the upload status
-	#	blocksize	The size of the block to send - Default 256 KiB
-	#	maxSpeed	Speed in KB/s to mantain during transfer - Default 0 -> No limit
-	def uploadFile(self, filePath, blocksize = 262144, maxSpeed = 0):
-		self.writtenSize = 0
-		self.maxSpeed = maxSpeed * 1024
+
+	def uploadFile(self, file_path, block_size=262144, max_speed=0):
+		"""
+		Upload a file - Return the upload status
+		block_size	The size of the block to send - Default 256 KiB
+		maxSpeed	Speed in KB/s to maintain during transfer - Default 0 -> No limit
+		"""
+		self.maxSpeed = max_speed * 1024
 		if not self.maxSpeed:
 			print("Transferring at maximum speed")
 		self.uploadStart = time.time()
-		name = os.path.basename(filePath).encode("utf-8", "ignore").decode("latin-1", "ignore")
-		with open(filePath, 'rb') as fp:
+		name = os.path.basename(file_path).encode("utf-8", "ignore").decode("latin-1", "ignore")
+		with open(file_path, 'rb') as fp:
 			try:
-				self.connection.storbinary('STOR '+name, fp, blocksize, self.throttler)
-				if (self.logging): 
-					self.logging.info("FtpUploader - uploadFile - Uploaded ["+name+"] - ["+filePath+"]")
-					print("FtpUploader - uploadFile - Uploaded ["+name+"] - ["+filePath+"]")
-				return True;
+				self.connection.storbinary('STOR ' + name, fp, block_size, self.throttler)
+				if self.logging:
+					self.logging.info("FtpUploader - uploadFile - Uploaded [" + name + "] - [" + file_path + "]")
+					print("FtpUploader - uploadFile - Uploaded [" + name + "] - [" + file_path + "]")
+				return True
 			except ftplib.error_perm:
-				self.logging.warn("FtpUploader - uploadFile - Permanent error during upload ["+name+"] - ["+filePath+"]")
+				self.logging.warn("FtpUploader - uploadFile - Permanent error during upload [" + name + "] - [" + file_path + "]")
 			except ftplib.error_temp:
-				self.logging.warn("FtpUploader - uploadFile - Temporary error during upload ["+name+"] - ["+filePath+"]")
-		return False;
+				self.logging.warn("FtpUploader - uploadFile - Temporary error during upload [" + name + "] - [" + file_path + "]")
+		return False
 	
 	total_length = 0
 	start_time = time.time()
@@ -72,12 +82,12 @@ class FtpUploader:
 		cycles = sleepSeconds/sleepTime
 		while self.writtenSize / (time.time() - self.uploadStart) > self.maxSpeed:
 			time.sleep(sleepTime)
-			i+=1
-			if not i%(cycles):
+			i += 1
+			if not i % cycles:
 				print("Elapsed: "+str(time.time() - self.uploadStart)+"s, started at "+str(self.uploadStart)+" and written "+str(self.writtenSize/1024/1024)+" MB Sleeping since "+str(sleepSeconds)+" seconds")
 				self.logging.info("Elapsed: "+str(time.time() - self.uploadStart)+"s, started at "+str(self.uploadStart)+" and written "+str(self.writtenSize/1024/1024)+" MB Sleeping since "+str(sleepSeconds)+" seconds")
 	
 	#Tries to gracefully exit
 	def __del__(self):
 		self.connection.quit()
-		self.logging.info("FtpUploader - __del__ - Quitted gracefully")
+		self.logging.info("FtpUploader - __del__ - Quit gracefully")
